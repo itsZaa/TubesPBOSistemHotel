140d48496712d5e90abde691ca81baa832dcc9f3
Marcel Andrean <marcel.andrean2003@gmail.com>
2023-07-03 01:12:23 +0700

    Update database & payment controller

--- src/controller/DatabaseController.java
+++ src/controller/DatabaseController.java
@@ -99,4 +99,63 @@ public class DatabaseController {
             return (false);
         }
     }
+
+    // UPDATE
+    public boolean updateUser(User user, String oldUsername) {
+        conn.connect();
+        String query = "UPDATE users SET Username='" + user.getUsername() + "', "
+                + "full_name='" + user.getFullname() + "', "
+                + "Email='" + user.getEmail() + "', "
+                + "Password='" + user.getPassword() + "', "
+                + "Address='" + user.getAddress() + "', "
+                + "phone_number='" + user.getPhoneNumber() + "', "
+                + "Type='" + user.getType().name()+ "' "
+                + "WHERE Username='" + oldUsername + "'";
+        try {
+            Statement stmt = conn.con.createStatement();
+            stmt.executeUpdate(query);
+            return (true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return (false);
+        }
+    }
+
+    // DELETE
+    public static boolean deleteUser(String username) {
+        conn.connect();
+
+        String query = "DELETE FROM users WHERE Username='" + username + "'";
+        try {
+            Statement stmt = conn.con.createStatement();
+            stmt.executeUpdate(query);
+            return (true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return (false);
+        }
+    }
+
+    // //GET TRANSAKSI
+    // public boolean insertRoomTransaction(String username, Transaction transaction) {
+    //     conn.connect();
+    //     String query = "INSERT INTO users VALUES(?,?,?,?,?,?,?,?)";
+    //     try {
+    //         PreparedStatement stmt = conn.con.prepareStatement(query);
+    //         stmt.setString(1, user.getUsername());
+    //         stmt.setString(2, user.getFullname());
+    //         stmt.setString(3, user.getEmail());
+    //         stmt.setString(4, user.getPassword());
+    //         stmt.setString(5, user.getAddress());
+    //         stmt.setString(6, user.getGender().name());
+    //         stmt.setString(7, user.getPhoneNumber());
+    //         stmt.setString(8, user.getType().name());
+
+    //         stmt.executeUpdate();
+    //         return (true);
+    //     } catch (SQLException e) {
+    //         e.printStackTrace();
+    //         return (false);
+    //     }
+    // }
 }
