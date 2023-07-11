a1420c01021be0f8fa1be213af8f4ab228456e22
scw22 <100103682+scw22@users.noreply.github.com>
2023-07-11 23:35:52 +0700

    SignInView
    Gui Sign In

--- src/controller/SignUpController.java
+++ src/controller/SignUpController.java
@@ -1,6 +1,6 @@
 package controller;
 
-public class RegisterController {
+public class SignUpController {
     // public String register(String userName, String fullName, String email, String password, String alamat, String male, String female, String noTelp) {
     //     String query;
     // }
